c21a2d2e8852e8d06d9eab2b29d43344b6a37c0f
NoNamedCobble <nexuspl500@gmail.com>
2025-05-05 17:52:01 +0200

    feat(backend): add userId validation for note operations and sort by newest first

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -5,18 +5,23 @@ export const createNote = async (req, res) => {
   const { userId } = req;
   const { title, content, background } = req.body;
   if (!title || !content || !background) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: "All fields are required." });
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "All fields are required." });
   }
 
   try {
     const newNote = new Note({ title, content, userId, background });
     await newNote.save();
-    res
-      .status(StatusCodes.CREATED)
-      .json({ message: "Note created successfully.", note: { _id: newNote._id, title, content, background } });
+    res.status(StatusCodes.CREATED).json({
+      message: "Note created successfully.",
+      note: { _id: newNote._id, title, content, background },
+    });
   } catch (error) {
     console.error("CreateNoteError: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
@@ -27,12 +32,16 @@ export const getNotes = async (req, res) => {
 
   const query = {
     userId,
-    $or: [{ title: { $regex: search, $options: "i" } }, { content: { $regex: search, $options: "i" } }],
+    $or: [
+      { title: { $regex: search, $options: "i" } },
+      { content: { $regex: search, $options: "i" } },
+    ],
   };
 
   try {
     const notes = await Note.find(query)
       .select("title content background")
+      .sort({ createdAt: -1 })
       .skip((page - 1) * pageSize)
       .limit(pageSize);
 
@@ -43,59 +52,83 @@ export const getNotes = async (req, res) => {
     res.status(StatusCodes.OK).json({ nextPage, notes });
   } catch (error) {
     console.error("GetNoteError: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
 export const getNoteById = async (req, res) => {
+  const { userId } = req;
   const { id } = req.params;
   try {
-    const note = await Note.findById(id).select("title content background");
+    const note = await Note.findOne({ _id: id, userId }).select(
+      "title content background"
+    );
     if (!note) {
-      return res.status(StatusCodes.NOT_FOUND).json({ message: "Note not found." });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Note not found." });
     }
 
     res.status(StatusCodes.OK).json(note);
   } catch (error) {
     console.error("GetNoteById: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
 export const updateNoteById = async (req, res) => {
+  const { userId } = req;
   const { id } = req.params;
   const { title, content, background } = req.body;
 
   try {
-    const updatedNote = await Note.findByIdAndUpdate(
-      id,
+    const updatedNote = await Note.findOneAndUpdate(
+      { _id: id, userId },
       { title, content, background },
       { new: true, runValidators: true }
     );
 
     if (!updatedNote) {
-      return res.status(StatusCodes.NOT_FOUND).json({ message: "Note not found." });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Note not found." });
     }
 
-    res.status(StatusCodes.OK).json({ message: "Note updated successfully.", note: updatedNote });
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "Note updated successfully.", note: updatedNote });
   } catch (error) {
     console.error("UpdateNoteById: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
 export const deleteNoteById = async (req, res) => {
+  const { userId } = req;
   const { id } = req.params;
+
   try {
-    const deletedNote = await Note.findByIdAndDelete(id);
+    const deletedNote = await Note.findOneAndDelete({ _id: id, userId });
 
     if (!deletedNote) {
-      return res.status(StatusCodes.NOT_FOUND).json({ message: "Note not found." });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Note not found." });
     }
 
-    res.status(StatusCodes.OK).json({ message: "Note deleted successfully.", note: deletedNote });
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "Note deleted successfully.", note: deletedNote });
   } catch (error) {
     console.error("DelteNoteById: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,27 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
 
-import { generateAccessToken, generateRefreshToken, verifyRefreshToken } from "../utils/jwtUtils.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken,
+} from "../utils/jwtUtils.js";
 
 export const createUser = async (req, res) => {
   const { email, nickname, password } = req.body;
 
   if (!email || !nickname || !password) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: "All fields are required." });
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "All fields are required." });
   }
 
   try {
     const existingUser = await User.findOne({ $or: [{ email }, { nickname }] });
     if (existingUser) {
-      return res.status(StatusCodes.CONFLICT).json({ message: "A user with this email or nickname already exists." });
+      return res.status(StatusCodes.CONFLICT).json({
+        message: "A user with this email or nickname already exists.",
+      });
     }
 
     const newUser = new User({ email, nickname, password });
@@ -21,20 +29,26 @@ export const createUser = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ message: "User has been created." });
   } catch (error) {
     console.log("CreateUserError: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: "All fields are required." });
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "All fields are required." });
   }
 
   try {
     const user = await User.findOne({ email });
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ message: "Invalid email or password." });
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Invalid email or password." });
     }
     const { _id } = user;
     const accessToken = generateAccessToken({ _id });
@@ -46,21 +60,23 @@ export const login = async (req, res) => {
     res.cookie("access_token", accessToken, {
       httpOnly: true,
       secure: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      maxAge: process.env.COOKIES_EXPIRES_IN,
       path: "/",
     });
 
     res.cookie("refresh_token", refreshToken, {
       httpOnly: true,
       secure: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      maxAge: process.env.COOKIES_EXPIRES_IN,
       path: "/",
     });
 
     res.status(StatusCodes.OK).json({ message: "Tokens set in cookies" });
   } catch (error) {
     console.log("LoginError: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
@@ -68,7 +84,10 @@ export const logout = async (req, res) => {
   const { userId } = req;
   try {
     const user = await User.findById(userId);
-    if (!user) return res.status(StatusCodes.NOT_FOUND).json({ message: "User not found." });
+    if (!user)
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "User not found." });
 
     user.refreshToken = "";
     await user.save();
@@ -90,7 +109,9 @@ export const logout = async (req, res) => {
     res.status(StatusCodes.OK).json({ message: "Successfully logged out." });
   } catch (error) {
     console.log("LogoutError: ", error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Server error. Please try again later." });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
@@ -102,18 +123,22 @@ export const refreshAccessToken = async (req, res, next) => {
 
     const user = await User.findById(_id);
     if (!user) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ message: "User not found." });
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "User not found." });
     }
 
     if (user.refreshToken === "") {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ message: "You are logout." });
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "You are logout." });
     }
 
     const newAccessToken = generateAccessToken({ _id });
     res.cookie("access_token", newAccessToken, {
       httpOnly: true,
       secure: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      maxAge: process.env.COOKIES_EXPIRES_IN,
       path: "/",
     });
 
@@ -121,6 +146,8 @@ export const refreshAccessToken = async (req, res, next) => {
     next();
   } catch (error) {
     console.log("RefreshAccessTokenError:", error);
-    res.status(StatusCodes.UNAUTHORIZED).json({ message: "Invalid access token." });
+    res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Invalid access token." });
   }
 };
