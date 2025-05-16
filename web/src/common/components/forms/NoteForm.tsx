e1b5d2de93e1549d33ea43e6ec7e9c4ec4ea8c16
NoNamedCobble <nexuspl500@gmail.com>
2025-05-16 11:55:29 +0200

    fix(frontend): preserve correct heading during exit animation

--- web/src/common/components/forms/NoteForm.tsx
+++ web/src/common/components/forms/NoteForm.tsx
@@ -3,51 +3,43 @@ import FormColorPicker from "@/common/components/FormColorPicker";
 import FormInput from "@/common/components/FormInput";
 import FormTextArea from "@/common/components/FormTextArea";
 import SubmitButton from "@/common/components/SubmitButton";
-import {
-  useCreateNoteMutation,
-  useUpdateNoteMutation,
-} from "@/common/hooks/useNotesHooks";
 import { useOutsideClick } from "@/common/hooks/useOutsideClick";
 import { noteSchema } from "@/common/schemas";
-import { NoteWithoutId } from "@/common/types";
-import { useNoteModalStore } from "@/store/useNoteModalStore";
+import { Note, NoteWithoutId } from "@/common/types";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMemo } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-export default function NoteForm() {
-  const { closeModal, currentNote } = useNoteModalStore();
-  const isEditMode = useMemo(() => Boolean(currentNote), [currentNote]);
+type NoteFormProps = {
+  currentNote: Note | null;
+  closeModal: () => void;
+  labels: {
+    heading: string;
+    submit: string;
+  };
+  mutate: (data: NoteWithoutId) => void;
+};
+
+export default function NoteForm({
+  currentNote,
+  closeModal,
+  labels,
+  mutate,
+}: NoteFormProps) {
   const { control, handleSubmit, reset, formState } = useForm<NoteWithoutId>({
     shouldUnregister: true,
     resolver: zodResolver(noteSchema),
     mode: "onSubmit",
-    defaultValues: {
-      title: currentNote?.title || "",
-      content: currentNote?.content || "",
-      background: currentNote?.background || "#ffffff",
+    defaultValues: currentNote ?? {
+      title: "",
+      content: "",
+      background: "#ffffff",
     },
   });
   const { isSubmitting } = formState;
-  const formLabels = {
-    heading: isEditMode ? "Edit your note" : "Create new note",
-    submit: isEditMode ? "Save changes" : "Create",
-  };
-
-  const createNoteMutation = useCreateNoteMutation();
-  const updateNoteMutation = useUpdateNoteMutation();
-
   const ref = useOutsideClick<HTMLFormElement>(closeModal);
 
   const onSubmit: SubmitHandler<NoteWithoutId> = async (data) => {
-    if (isEditMode) {
-      if (currentNote) {
-        updateNoteMutation.mutate({ _id: currentNote._id, ...data });
-      }
-    } else {
-      createNoteMutation.mutate(data);
-    }
-
+    mutate(data);
     reset();
     closeModal();
   };
@@ -59,7 +51,7 @@ export default function NoteForm() {
       noValidate
       className="relative flex h-5/6 min-h-fit w-full max-w-xl flex-col gap-4 rounded-xl bg-secondary px-3 pt-4 shadow-custom-blue md:h-fit md:p-8"
     >
-      <h2 className="text-3xl text-primary">{formLabels.heading}</h2>
+      <h2 className="text-3xl text-primary">{labels.heading}</h2>
       <button
         onClick={closeModal}
         type="button"
@@ -83,7 +75,7 @@ export default function NoteForm() {
         />
         <FormColorPicker<NoteWithoutId> control={control} name="background" />
       </div>
-      <SubmitButton isSubmitting={isSubmitting} title={formLabels.submit} />
+      <SubmitButton isSubmitting={isSubmitting} title={labels.submit} />
     </form>
   );
 }
--- web/src/common/components/modals/NoteModal.tsx
+++ web/src/common/components/modals/NoteModal.tsx
@@ -1,11 +1,28 @@
 "use client";
 import NoteForm from "@/common/components/forms/NoteForm";
+import {
+  useCreateNoteMutation,
+  useUpdateNoteMutation,
+} from "@/common/hooks/useNotesHooks";
+import { NoteWithoutId } from "@/common/types";
 import { useNoteModalStore } from "@/store/useNoteModalStore";
 import { AnimatePresence, motion } from "framer-motion";
 import FocusLock from "react-focus-lock";
 
-export default function NewNoteModal() {
-  const { isOpen } = useNoteModalStore();
+export default function NoteModal() {
+  const { isOpen, currentNote, closeModal } = useNoteModalStore();
+  const updateNoteMutation = useUpdateNoteMutation();
+  const createNoteMutation = useCreateNoteMutation();
+
+  const labels = {
+    heading: currentNote ? "Edit your note" : "Create new note",
+    submit: currentNote ? "Save changes" : "Create",
+  };
+
+  const mutate = (data: NoteWithoutId) =>
+    currentNote
+      ? updateNoteMutation.mutate({ _id: currentNote._id, ...data })
+      : createNoteMutation.mutate(data);
 
   return (
     <AnimatePresence>
@@ -18,7 +35,12 @@ export default function NewNoteModal() {
             transition={{ duration: 0.2, ease: "easeInOut" }}
             className="fixed left-0 top-0 z-10 flex h-full w-full items-end justify-center bg-black bg-opacity-30 md:items-center"
           >
-            <NoteForm />
+            <NoteForm
+              currentNote={currentNote}
+              closeModal={closeModal}
+              labels={labels}
+              mutate={mutate}
+            />
           </motion.div>
         </FocusLock>
       )}
